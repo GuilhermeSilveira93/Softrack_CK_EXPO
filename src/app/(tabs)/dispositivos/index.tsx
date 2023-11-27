58b7da7c21913e372a8822e524ac9df0db659f28
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2023-11-27 10:26:02 -0300

    1

--- src/app/(tabs)/dispositivos/index.tsx
+++
@@ -1,7 +0,0 @@
-import DispositivosSalvos from "@/app/(Dispositivos)/DispositivosSalvos";
-export const Dispositivos = () => {
-  return (
-    <DispositivosSalvos />
-  );
-};
-export default Dispositivos;
