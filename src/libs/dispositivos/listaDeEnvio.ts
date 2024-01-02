f6939818a2b055fbb5005fe065457e6946f09ec2
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-01-02 17:02:44 -0300

    Delete listaDeEnvio.ts

--- src/libs/dispositivos/listaDeEnvio.ts
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
-export const FetchListaDeEnvio = async () => {
-  const devices = await AsyncStorage.getItem('listaDeEnvio').then((res) => res)
-  if (devices) {
-    return JSON.parse(devices)
-  }
-  return []
-}
